5ee0c0b56cec34db6d895bdc00ae1f3c5c969aed
christ <christallohan@gmail.com>
2023-05-15 18:30:29 +0100

    well

--- src/services/utils-class/router.ts
+++ src/services/utils-class/router.ts
@@ -11,11 +11,8 @@ export class ControllerRoutePayload<ServiceClass> {
 
 export function ROUTER_POST_MODEL(router: Router) {
     return (route: string, service: ControllerRoutePayload<any>) => {
-        console.log(service)
         return router.post(route, (async (req, res, next) => {
             let reqData = req.body.reqData;
-            console.log('Is Here 123')
-            console.log(reqData)
             await service.serviceClassInstance[service.service(service.serviceClassInstance).name]
                 (reqData, (response) => { res.send(response) });
         }))

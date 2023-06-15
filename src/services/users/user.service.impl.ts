3416085b634f1b137d4c25e4b59a9d11d19dd256
christ <christallohan@gmail.com>
2023-06-15 18:24:16 +0100

    well

--- src/services/users/user.service.impl.ts
+++ src/services/users/user.service.impl.ts
@@ -34,4 +34,37 @@ export class UserServiceImpl {
         console.log(output)
         callBack(output)
     }
+
+
+//     interface SuperbUser {
+//     userId: number;
+//     macAddress: string;
+//     username: string;
+//     email: string;
+//     password: string;
+//     firstName: string;
+//     lastName: string;
+//     roles: ('Admin' | 'Editor' | 'Author')[]
+// };
+
+// type Subscriber<___ extends $___, $___> = Pick<___, keyof $___>;
+
+// const subscriber: Subscriber<SellerIncommeStat, Toto> = { bebe: 'pfppp', papa: 'papap', age: 12 }
+
+// const subscriber2: Subscriber<SellerIncommeStat3, Tata> = { name: 'pfppp', age: 12 }
+
+// console.log(subscriber);
+// /*
+// {
+//   "userId": 4,
+//   "macAddress": "a:5ub:mach1ne",
+//   "username": "sub_user",
+//   "email": "sub_user@gmail.com",
+//   "password": "12345678",
+//   "firstName": "Abdullah",
+//   "lastName": "Numan"
+// }
+// */
+
+// console.log(subscriber.roles); // undefined
 }
